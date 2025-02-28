d18ab8111a3424b1019d375566335073aa32004f
JonathanDiaz12 <diaz.j023958@stu.sanjac.edu>
2025-02-28 04:58:51 +0000

    Added the factorial loop and the pyramid loop

--- main.cpp
+++ main.cpp
@@ -8,16 +8,8 @@ using namespace std;
 int main() {
     int Menu_Quit = 5;
     int Menu_Choice = 0; // Initialize Menu_Choice to a value that is not equal to Menu_Quit
-    int Number_Factorial;
-    int Number_Pyramid;
-    int Number_Sum;
-    string String_Reverse;
-    int String_Length;
-    int rows;
-    int count = 0;
-    int count1 = 0;
-    int k = 0;
-
+    int Factorize,f=1,i=1;
+    int row, space;
     // Use a while loop to keep the program running until the user chooses to quit
     while (Menu_Choice != Menu_Quit) {
         cout << "Interactive Utility Program" << endl;
@@ -31,5 +23,33 @@ int main() {
         cout << "Enter your choice: ";
         cin >> Menu_Choice;
         cout << endl;
-
+        if (Menu_Choice ==1)
+        {
+            cout<<"Enter the number to factorialize it"<<endl;
+            cin>>Factorize;
+            while(i<=Factorize)
+            {
+                f=f*i;
+                i++;
+            }
+            cout<<"The factorial of "<<Factorize<< "is: "<<f<<endl; 
+        }else if(Menu_Choice ==2){
+            cout<<"Enter the amount of rows for the pyramid: ";
+            cin>>row;
+            for(int i = 1, k = 0; i <= row; ++i, k = 0) {
+                for(space = 1; space <= row-i; ++space) {
+                    cout <<"  ";
+                }
+        
+                while(k != 2*i-1) {
+                    cout << k<<" ";
+                    ++k;
+                }
+                cout << endl;
+            }cout<<"==========================="<< endl;    
+        }else if(Menu_Choice==3){
+            
+        }
     }
+    return 0;
+}
